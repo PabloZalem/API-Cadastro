7efe4f9413791d0fde10f9a5a80dd8ccad62452e
Pablo Zalem <zalem.pablo@yahoo.com>
2025-10-22 19:17:28 -0300

    Criamos a nossa interface JPARepository, que sera o nosso facilitador

---
+++ src/main/java/infrastructure/repository/UsuarioRepository.java
@@ -0,0 +1,9 @@
+package infrastructure.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import infrastructure.entitys.Usuario;
+
+public interface UsuarioRepository extends JpaRepository<Usuario, Integer>{
+
+}
