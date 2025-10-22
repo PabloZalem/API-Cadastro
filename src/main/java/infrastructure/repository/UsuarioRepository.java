087388d917c7230ca022312e11b5fdbc70ba0bf9
Pablo Zalem <zalem.pablo@yahoo.com>
2025-10-22 19:50:20 -0300

    Teste

--- src/main/java/infrastructure/repository/UsuarioRepository.java
+++ src/main/java/infrastructure/repository/UsuarioRepository.java
@@ -4,6 +4,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import infrastructure.entitys.Usuario;
 
+
 public interface UsuarioRepository extends JpaRepository<Usuario, Integer>{
 
 }
