f7808f488451fc66394f515009ae25faaf1e52a3
Pablo Zalem <zalem.pablo@yahoo.com>
2025-10-22 19:55:58 -0300

    Criamos na nossa pasta business a classe service

---
+++ src/main/java/business/UsuarioService.java
@@ -0,0 +1,20 @@
+package business;
+
+import org.springframework.stereotype.Service;
+
+import infrastructure.entitys.Usuario;
+import infrastructure.repository.UsuarioRepository;
+
+@Service
+public class UsuarioService {
+	// Vamos fazer por injecao de dependencia
+	private final UsuarioRepository repository;
+	
+	public UsuarioService(UsuarioRepository repository) {
+		this.repository = repository;
+	}
+	
+	public void salvarUsuario(Usuario usuario) {
+		repository.saveAndFlush(usuario);
+	}
+}
