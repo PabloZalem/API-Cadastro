8db8c5bb46ab4c09d124232d16231ea47fd90528
Pablo Zalem <zalem.pablo@yahoo.com>
2025-10-26 14:37:04 -0300

    Criacao de um metodo findByEmail, utilizando Optional para evitar estourar uma Exception

--- src/main/java/infrastructure/repository/UsuarioRepository.java
+++ src/main/java/infrastructure/repository/UsuarioRepository.java
@@ -1,9 +1,13 @@
 package infrastructure.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import infrastructure.entitys.Usuario;
 
 
 public interface UsuarioRepository extends JpaRepository<Usuario, Integer>{
+	
+	Optional<Usuario> findByEmail(String email);
 }
