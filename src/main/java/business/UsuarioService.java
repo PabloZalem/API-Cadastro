327f26e4e8d2a0c71b7e1b7dc978bb1eb23ec250
Pablo Zalem <zalem.pablo@yahoo.com>
2025-10-27 19:29:48 -0300

    Criacao do metodo atualizar cadastro

--- src/main/java/business/UsuarioService.java
+++ src/main/java/business/UsuarioService.java
@@ -2,6 +2,7 @@ package business;
 
 import org.springframework.stereotype.Service;
 
+import dto.AtualizarUsuarioDTO;
 import infrastructure.entitys.Usuario;
 import infrastructure.repository.UsuarioRepository;
 
@@ -27,5 +28,17 @@ public class UsuarioService {
 	
 	public void deletarUsuarioPorEmail(String email) {
 		repository.deleteByEmail(email);
+	}
+	
+	public void atualizarUsuarioPorId(Integer id, Usuario usuario) {
+		Usuario usuarioEntity = repository.findById(id).orElseThrow(() -> new RuntimeException("Usuario não encontrado"));
+		
+		Usuario usuarioAtualizado = Usuario.builder()
+        .email(usuario.getEmail() != null ? usuario.getEmail() : usuarioEntity.getEmail())
+        .nome(usuario.getNome() != null ? usuario.getNome() : usuarioEntity.getNome())
+        .id(usuarioEntity.getId())
+        .build();
+		
+		repository.saveAndFlush(usuarioAtualizado);
 	}
 }
