3f362798ba174473fba82b841be35f9ac4a0337c
Pablo Zalem <zalem.pablo@yahoo.com>
2025-10-28 18:26:39 -0300

    Criacao do GetController

--- src/main/java/controller/UsuarioController.java
+++ src/main/java/controller/UsuarioController.java
@@ -1,9 +1,11 @@
 package controller;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import business.UsuarioService;
@@ -20,5 +22,10 @@ public class UsuarioController {
 	public ResponseEntity<Void> salvarUsuario(@RequestBody Usuario usario) {
 		service.salvarUsuario(usario);
 		return ResponseEntity.ok().build();
+	}
+	
+	@GetMapping
+	public ResponseEntity<Usuario> buscarUsuarioPorEmail(@RequestParam String email) {
+		return ResponseEntity.ok(service.buscarUsuarioPorEmail(email));
 	}
 }
