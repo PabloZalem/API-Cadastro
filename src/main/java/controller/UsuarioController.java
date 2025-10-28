9c6b6b84b1e29038acb961b4d38c1e57f62a8b9d
Pablo Zalem <zalem.pablo@yahoo.com>
2025-10-28 18:07:51 -0300

    Criacao do controller para salvar usaurio

---
+++ src/main/java/controller/UsuarioController.java
@@ -0,0 +1,24 @@
+package controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import business.UsuarioService;
+import infrastructure.entitys.Usuario;
+import lombok.RequiredArgsConstructor;
+
+@RequestMapping("/usuario")
+@RestController
+@RequiredArgsConstructor
+public class UsuarioController {
+	private final UsuarioService service;
+	
+	@PostMapping
+	public ResponseEntity<Void> salvarUsuario(@RequestBody Usuario usario) {
+		service.salvarUsuario(usario);
+		return ResponseEntity.ok().build();
+	}
+}
